0aea42d51b84372ed83db760bf45d5f1b7f6c5c5
danghuutrithuc <danghuutrithuc@gmail.com>
2025-05-30 01:07:22 +0700

    add lienhe page and display UI

--- view/lienhe.php
+++ view/lienhe.php
@@ -1,7 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-commerce</title>
+    <link rel="stylesheet" href="view/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" integrity="sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=" crossorigin="anonymous">
+</head>
+<body>
 <div class="row mb">
-    <div class="boxtieude">LIÊN HỆ</div>
-        <div class="boxnoidung row">
-            
+        <div class="boxnoidung-contact lien_he row mb">
+            <h1>Chào mừng bạn đến trung tâm trợ giúp<br> ThucFood Việt Nam</h1>
+            <p>ThucFood có thể giúp gì cho bạn hôm nay?</p>
+            <div class="contact-search ">
+                <i class="fa fa-search search-icon" aria-hidden="true"></i>
+                <input type="search" name="query" id="query" placeholder="Tìm kiếm" autocomplete="off" aria-label="Search"></input>
+            </div>
         </div>
-    
+
+        <div class="row box-lienhe lien_he-grid mr5">
+            <div class="block">
+                <div class="gridicon-circle">
+                    <i class="fa fa-user-circle user_icon_circle"></i> 
+                </div>
+                    <div>
+                        <p class="text-account">Tài khoản & Hồ sơ</p>
+                        <p class="text-p-details"> Xem chi tiết →</p>
+                    </div>
+            </div>
+        </div>
+        <div class="row box-lienhe lien_he-grid mr5">
+            <div class="block">
+                <div class="gridicon-circle">
+                    <i class="fa fa-user-circle user_icon_circle"></i> 
+                </div>
+                    <div>
+                        <p class="text-account">Quản lý Đơn hàng</p>
+                        <p class="text-p-details"> Xem chi tiết →</p>
+                    </div>
+            </div>
+        </div>
+
+        <div class="row box-lienhe lien_he-grid">
+            <div class="block">
+                <div class="gridicon-circle">
+                    <i class="fa fa-user-circle user_icon_circle"></i> 
+                </div>
+                    <div>
+                        <p class="text-account">Thanh toán</p>
+                        <p class="text-p-details"> Xem chi tiết →</p>
+                    </div>
+            </div>
+        </div>
+
+        <div class="row box-lienhe lien_he-grid mr5">
+            <div class="block">
+                <div class="gridicon-circle">
+                    <i class="fa fa-user-circle user_icon_circle"></i> 
+                </div>
+                    <div>
+                        <p class="text-account">Câu hỏi thường gặp (FAQs)</p>
+                        <p class="text-p-details"> Xem chi tiết →</p>
+                    </div>
+            </div>
+        </div>
+
+        <div class="row box-lienhe lien_he-grid mr5">
+            <div class="block">
+                <div class="gridicon-circle">
+                    <i class="fa fa-user-circle user_icon_circle"></i> 
+                </div>
+                    <div>
+                        <p class="text-account">Cơ hội nghề nghiệp tại ThucFood</p>
+                        <p class="text-p-details"> Xem chi tiết →</p>
+                    </div>
+            </div>
+        </div>
+
+        <div class="row box-lienhe lien_he-grid">
+            <div class="block">
+                <div class="gridicon-circle">
+                    <i class="fa fa-user-circle user_icon_circle"></i> 
+                </div>
+                    <div>
+                        <p class="text-account">Bảo mật & Quyền riêng tư dữ liệu</p>
+                        <p class="text-p-details"> Xem chi tiết →</p>
+                    </div>
+            </div>
+        </div>
+
+        <h2 class="home-title">Liên hệ</h2>
+        <br>
+        <div class="contact-box mr">
+            <div class="row">
+                <div class="boxnoidung-info ">
+                    <div class="home-title2">Trụ sở chính</div>
+                    <p>
+                        Địa chỉ:Tầng 3, Số 183 Âu Cơ, Phường 9, Quận Tân Bình, Thành phố Hồ Chí Minh, Việt Nam 
+                    </p>
+                    <p>Điện thoại: (+84) 886192294</p>
+                </div>
+            </div>
+            <div class="row ">
+                <div class="boxnoidung-info">
+                    <div class="home-title2">Chăm sóc khách hàng</div>
+                    <button class="btn-gopy">Góp ý</button>
+                    <button class="btn-gopy">Gửi tin nhắn</button>
+                    <button class="btn-gopy">Gọi tổng đài</button>
+                </div>
+            </div>
+        </div>    
 </div>
+</body>
